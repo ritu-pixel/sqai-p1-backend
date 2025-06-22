ab70134597a3991c302c67e8bde3bdb2879d4b90
Ritu Rajput <75141887+ritu-pixel@users.noreply.github.com>
2025-06-22 23:41:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from db.database import Base, engine
 from routers import users, files, extracted
 import uvicorn
+import os
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
-
-Base.metadata.create_all(bind=engine)
-
+# Initialize FastAPI app
 app = FastAPI(title="Invoice Management API")
 
-# CORS settings as frontend is separate
+# CORS settings for frontend
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -26,6 +22,15 @@ app.include_router(users.router)
 app.include_router(files.router)
 app.include_router(extracted.router)
 
+# Health check endpoint
 @app.get("/check", tags=["health"])
 def root():
     return {"message": "API is running"}
+
+# Move DB table creation to here, inside __main__ for dev only
+if __name__ == "__main__":
+    # Create database tables for local dev only
+    Base.metadata.create_all(bind=engine)
+
+    port = int(os.environ.get("PORT", 8000))  # Default to 8000 locally
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
